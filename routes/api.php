8e7b0933d2d002d458979f471c792226338e2abf
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-09-07 23:31:10 +0700

    config auth and added dummy

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//
+
+Route::prefix('v1')->as('v1')->group(function () {
+    Route::get('test-connection', function () {
+        return response()->json(
+            [
+                'status' =>  'Connection established',
+            ]
+        );
+    });
+
+    Route::prefix('auth')->group(function () {
+        Route::post('/login', [LoginController::class, 'login']);
+    });
 });
