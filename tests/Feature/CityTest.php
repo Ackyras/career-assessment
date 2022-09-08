cfeb86c344869531b838989230ae390fcffc6294
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-09-08 19:14:23 +0700

    add unit testing

---
+++ tests/Feature/CityTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\City;
+use App\Http\Resources\CityResource;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Repositories\API\CityRepository as APICityRepository;
+use App\Repositories\Database\CityRepository as DatabaseCityRepository;
+
+class CityTest extends TestCase
+{
+    public function test_get_all_citiy_with_database()
+    {
+        $DatabaseRepository = new DatabaseCityRepository();
+
+        $queries = [];
+
+        $cities = $DatabaseRepository->get($queries);
+
+        $cities_count = City::count();
+
+        $this->assertTrue(count($cities) == $cities_count);
+    }
+
+    public function test_check_if_a_data_exists_from_database()
+    {
+        $city = new CityResource(City::inRandomOrder()->first());
+
+        $DatabaseRepository = new DatabaseCityRepository();
+
+        $queries = [];
+
+        $cities = $DatabaseRepository->get($queries);
+
+        foreach ($cities as $key => $value) {
+            if ($value['city_id'] == $city['city_id'] && $value['city'] == $city['city']) {
+                $this->assertTrue(true);
+            }
+        }
+    }
+
+    public function test_get_all_citiy_with_direct_api()
+    {
+        $APIRepository = new APICityRepository();
+
+        $queries = [];
+
+        $cities = $APIRepository->get($queries);
+
+        $cities_count = City::count();
+
+        if (count($cities) == $cities_count) {
+            $this->assertTrue(true);
+        }
+    }
+
+    public function test_check_if_api_and_database_output_is_same()
+    {
+        $DatabaseRepository = new DatabaseCityRepository();
+        $APIRepository = new APICityRepository();
+
+        $queries = [];
+
+        $APIcities = $APIRepository->get($queries);
+        $Databasecities = $DatabaseRepository->get($queries);
+        if (count($APIcities) == count($Databasecities)) {
+            $this->assertTrue(true);
+        }
+    }
+}
---
+++ tests/Feature/ProvinceTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Resources\ProvinceResource;
+use App\Models\Province;
+use App\Repositories\API\ProvinceRepository as APIProvinceRepository;
+use App\Repositories\Database\ProvinceRepository as DatabaseProvinceRepository;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProvinceTest extends TestCase
+{
+    public function test_get_all_province_with_database()
+    {
+        $DatabaseRepository = new DatabaseProvinceRepository();
+
+        $queries = [];
+
+        $provinces = $DatabaseRepository->get($queries);
+
+        $provinces_count = Province::count();
+
+        $this->assertTrue(count($provinces) == $provinces_count);
+    }
+
+    public function test_check_if_a_data_exists_from_database()
+    {
+        $province = new ProvinceResource(Province::inRandomOrder()->first());
+
+        $DatabaseRepository = new DatabaseProvinceRepository();
+
+        $queries = [];
+
+        $provinces = $DatabaseRepository->get($queries);
+
+        foreach ($provinces as $key => $value) {
+            if ($value['province_id'] == $province['province_id'] && $value['province'] == $province['province']) {
+                $this->assertTrue(true);
+            }
+        }
+    }
+
+    public function test_get_all_province_with_direct_api()
+    {
+        $APIRepository = new APIProvinceRepository();
+
+        $queries = [];
+
+        $provinces = $APIRepository->get($queries);
+
+        $provinces_count = Province::count();
+
+        if (count($provinces) == $provinces_count) {
+            $this->assertTrue(true);
+        }
+    }
+
+    public function test_check_if_api_and_database_output_is_same()
+    {
+        $DatabaseRepository = new DatabaseProvinceRepository();
+        $APIRepository = new APIProvinceRepository();
+
+        $queries = [];
+
+        $APIprovinces = $APIRepository->get($queries);
+        $Databaseprovinces = $DatabaseRepository->get($queries);
+        if (count($APIprovinces) == count($Databaseprovinces)) {
+            $this->assertTrue(true);
+        }
+    }
+}
